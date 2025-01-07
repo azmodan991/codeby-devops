dc47e1bef62880c5546d9ad52a96d5df34bff21d
daniil <osipov.dro@dvfu.ru>
2025-01-07 10:46:50 +1000

    change Apps for builds

--- lesson26/HelloDevOps/src/main/java/example/App.java
+++ lesson26/HelloDevOps/src/main/java/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps!");
+        System.out.println("Hello DevOps1!");
     }
 }
